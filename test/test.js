cbdd9eaabdfa9ce6722b2ae8079f235991b5a535
Neeraj Verma <neerajverma101@gmail.com>
2023-03-25 15:56:21 +0000

    Pending changes exported from your codespace

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
-var assert = require('assert');
+const assert = require('assert');
 const request = require('request');
+const express = require("express");
+
+const app = express();
 
 describe('Weather API', () => {
   it('should return a response of 200 when the API is running', () => {
@@ -8,11 +11,12 @@ describe('Weather API', () => {
       status: jest.fn().mockReturnValue({
         send: jest.fn()
       })
-    };
+    }
     app.get('/', (req, res) => {
       res.status(200).send('Weather API is running');
     });
- it('should return second charachter of the string', function () {
-        assert.equal("Weather API is Running!".charAt(1), 'e');
+    it('should return second charachter of the string', function () {
+      assert.equal("Weather API is Running!".charAt(1), 'e');
     });
-});
+  })
+})
